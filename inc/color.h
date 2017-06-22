4401ad72e3546cdeddcffcdd6eb68e6a3f7cd41c
colelyman <cole@colelyman.com>
2017-06-21 20:00:46 -0600

    Created the Color and ColorManager header files.

---
+++ inc/color.h
@@ -0,0 +1,93 @@
+/**
+ * @class Color
+ *
+ * Color is a class that represents a single sample or species, which make up
+ * the colored de Bruijn graph. This class is essentially a wrapper for the
+ * DBGQuery class in the dbgfm package.
+ *
+ * @author Cole Lyman
+ *
+ * @date 2017/6/21
+ *
+ */
+
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <string>
+
+#include "fm_index.h"
+#include "dbg_query.h"
+
+using std::string;
+
+class Color {
+
+    public:
+        Color(string name, string pathToFMIndex);
+
+        ~Color();
+
+        /*
+         * The following methods are wrappers around the methods found in DBGQuery.
+         */
+        
+        /// Returns whether or not s is a vertex of the color 
+        bool isVertex(string& s);
+
+        /// Returns whether or not there is an outgoing edge, b, present for node s
+        bool isPrefixNeighbor(string& s, char b);
+
+        /// Returns whether or not there is an incoming edge, b, present for node s
+        bool isSuffixNeighbor(string&s, char b);
+
+        /// Returns the outgoing edges of s. 
+        /// @todo figure out what this really returns
+        string getSuffixNeighbors(string& s);
+
+        /// Returns the incoming edges of s. 
+        /// @todo figure out what this really returns
+        string getPrefixNeighbors(string& s);
+
+        /**
+         * Get the substring and index of a substring in the color.
+         * @param idx the position of where the substring begins
+         * @param len how long the substring is
+         * @return a pair, where first is the substring, and second is the index
+         */
+        pair<string, size_t> extractSubstringAndIndex(size_t idx, size_t len);
+
+        /// Returns the substring at index for a certain length
+        string extractSubstring(size_t idx, size_t len);
+
+        /*
+         * Getters
+         */
+
+        /// Return the name of the color
+        string getName();
+
+        /// Return the ID of the color
+        int getID();
+
+        /// Return the FMIndex of the color
+        FMIndex* getFMIndex();
+
+    private:
+        /// The name of the color
+        string name;
+
+        /// The ID of the color
+        int id;
+
+        /**
+         * The pointer to the instance of the FMIndex. This is where
+         * all of the data for the color are stored.
+         */
+        FMIndex* fmIndex;
+
+        /// The instance of DBGQuery that provides access into the FMIndex
+        DBGQuery dbg;
+};
+
+#endif // COLOR_H
---
+++ inc/color_manager.h
@@ -0,0 +1,57 @@
+/**
+ * @class ColorManager
+ *
+ * ColorManager is a class that holds all of the colors, and manages when they
+ * will be created and destroyed. 
+ *
+ * @author Cole Lyman
+ *
+ * @date 2017/6/21
+ *
+ */
+
+#ifndef COLOR_MANAGER_H
+#define COLOR_MANAGER_H
+
+#include <string>
+#include <map>
+
+#include "color.h"
+
+using std::string;
+using std::map;
+
+class ColorManager {
+
+    public:
+        ColorManager();
+
+        ~ColorManager();
+
+        /**
+         * Adds a color to the de Bruijn graph.
+         * @param colorName a human readable name (preferably with no whitespace) for the color
+         * @param the path to the BWT file to load into the FMIndex
+         * @returns the instance of the color after it is added
+         */
+        Color* addColor(string& colorName, string& pathToBWT);
+
+        /// Returns the color given a colorID
+        Color* getColor(int colorID);
+
+    private:
+        /// Removes all colors and properly deallocates them
+        void removeAllColors();
+
+        // colors is where each Color is located.
+        // @key is of type int and represents the id of the Color.
+        // @value is of type Color*.
+        map<int, Color*> colors;
+
+        // numColors is the total number of colors added.
+        // It also determines the ID of each Color.
+        int numColors;
+
+};
+
+#endif // COLOR_MANAGER_H

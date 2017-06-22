9b46a82a6cb812416dae280584ec74e536a84640
colelyman <cole@colelyman.com>
2017-06-22 17:44:13 -0600

    Added empty main function.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * This is the entry point into kleuren. It basically calls a
+ * function to parse the arguments and then passes all of the 
+ * logic off to the Driver.
+ *
+ * @author Cole Lyman
+ *
+ * @date 2017/6/22
+ */
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}

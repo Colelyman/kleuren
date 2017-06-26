1daf64fc6532cc788aaeba64d2b7f33e4adb7198
Cole Lyman <cole@colelyman.com>
2017-06-26 14:55:48 -0600

    Implemented Color.

--- src/color.cpp
+++ src/color.cpp
@@ -13,41 +13,41 @@ Color::~Color() {
 }
 
 bool Color::isVertex(string& s) {
-
+    return DBGQuery::isVertex(fmIndex, s);
 }
 
 bool Color::isPrefixNeighbor(string& s, char b) {
-
+    return DBGQuery::isPrefixNeighbor(fmIndex, s, b);
 }
 
 bool Color::isSuffixNeighbor(string& s, char b) {
-
+    return DBGQuery::isSuffixNeighbor(fmIndex, s, b);
 }
 
 string Color::getSuffixNeighbors(string& s) {
-
+    return DBGQuery::getSuffixNeighbors(fmIndex, s);
 }
 
 string Color::getPrefixNeighbors(string& s) {
-
+    return DBGQuery::getPrefixNeighbors(fmIndex, s);
 }
 
 pair<string, size_t> Color::extractSubstringAndIndex(size_t idx, size_t len) {
-
+    return DBGQuery::extractSubstringAndIndex(fmIndex, idx, len);
 }
 
 string Color::extractSubstring(size_t idx, size_t len) {
-
+    return DBGQuery::extractSubstring(fmIndex, idx, len);
 }
 
 string Color::getName() {
-
+    return name;
 }
 
 int Color::getID() {
-
+    return id;
 }
 
 FMIndex* Color::getFMIndex() {
-
+    return fmIndex;
 }

9918e9b33711e69d2c68d9c6e0663b5a4d874c3c
Cole Lyman <cole@colelyman.com>
2017-07-03 11:49:10 -0600

    Fixed silly error in the findEndKmer test case.

--- test/src/bubble_builder.cpp
+++ test/src/bubble_builder.cpp
@@ -9,15 +9,16 @@
 TEST_CASE("BubbleBuilder findEndKmer", "[bubble]") {
     BubbleBuilder bb = BubbleBuilder();
     string pathToToy1 = "./data/toy1.bwtdisk";
-    string pathtoToy2 = "./data/toy2.bwtdisk";
+    string pathToToy2 = "./data/toy2.bwtdisk";
     Color toyColor1 = Color(1, "toyColor1", pathToToy1);
     Color toyColor2 = Color(2, "toyColor2", pathToToy2);
 
-    ColorSet colors = ColorSet(set<Color*>({&toyColor1, &toyColor2});
+    ColorSet colors = ColorSet(set<Color*>({&toyColor1, &toyColor2}));
 
     SECTION("Finding a simple endKmer of two colors with a single SNP") {
         string startKmer = "ACT";
-        string endKmer = "TAC";
-        REQUIRE(strcmp(bb.findEndKmer(startKmer, color, colors), endKmer) == 0);
+        const char* endKmer = "TAC";
+        REQUIRE(strcmp(bb.findEndKmer(startKmer, &toyColor1, colors).c_str(), endKmer) == 0);
+        REQUIRE(strcmp(bb.findEndKmer(startKmer, &toyColor2, colors).c_str(), endKmer) == 0);
     }
 }

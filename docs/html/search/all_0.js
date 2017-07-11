69643afa4fae2d3e437469291ef112c3e81f1052
colelyman <cole@colelyman.com>
2017-07-11 11:21:11 -0600

    Added initial documentation through doxygen.

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['kleuren',['kleuren',['../md_README.html',1,'']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "k",
+  1: "k"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+

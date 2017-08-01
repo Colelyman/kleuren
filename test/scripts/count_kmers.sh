bcc2f8299fd46e0feb7236db2500f2932f3a408e
colelyman <cole@colelyman.com>
2017-08-01 15:33:52 -0600

    Added initial results and scripts for testing.

--- test/scripts/count_kmers.sh
+++ test/scripts/count_kmers.sh
@@ -11,14 +11,14 @@ else
     exit
 fi
 
-GENOMES=../data/ebola.fa,../data/flu.fa,../data/marburg.fa,../data/zika.fa
-KMER_SIZE=17
-H5_FILE=../data/super_viruses.count.h5
+GENOMES=$(ls -m ../data/sim_population/generation-5/*.fa | sed -e 's/ //g;:a;N;$!ba;s/\n//g')
+KMER_SIZE=13
+H5_FILE=../data/sim_population/sim_population.count.h5
 # run DSK on all of the genomes
 echo "Count the kmers"
 $DSK -file $GENOMES -kmer-size $KMER_SIZE -solidity-kind one -out $H5_FILE -abundance-min 1
 
-OUT_FILE=../data/super_viruses.count.txt
+OUT_FILE=../data/sim_population/sim_population.count.txt
 # dump the kmer counts to a text file
 echo "Convert the binary output file to a text file"
 $DSK2ASCII -file $H5_FILE -out $OUT_FILE

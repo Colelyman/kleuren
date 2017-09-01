19f5d647b2d6a57f161bcf99fdd2620bdcda6396
colelyman <cole@colelyman.com>
2017-09-01 16:07:10 -0600

    Updated the raw pointers to shared_ptr.

--- test/src/color_set.cpp
+++ test/src/color_set.cpp
@@ -6,17 +6,15 @@
 
 #include "color_set.h"
 
-using std::make_shared;
-
 TEST_CASE("ColorSet tests", "[colors]") {
     string pathToFMIndex1 = "./data/zika.bwtdisk";
     string pathToFMIndex2 = "./data/toy1.bwtdisk";
     string pathToFMIndex3 = "./data/toy2.bwtdisk";
-    Color color1 = Color(1, "zika", pathToFMIndex1);
-    Color color2 = Color(2, "toy1", pathToFMIndex2);
-    Color color3 = Color(3, "toy2", pathToFMIndex3);
+    shared_ptr<Color> color1(new Color(1, "zika", pathToFMIndex1));
+    shared_ptr<Color> color2(new Color(2, "toy1", pathToFMIndex2));
+    shared_ptr<Color> color3(new Color(3, "toy2", pathToFMIndex3));
 
-    ColorSet colors = ColorSet(set<shared_ptr<Color> >({make_shared<Color>(color1), make_shared<Color>(color2), make_shared<Color>(color3)}));
+    ColorSet colors = ColorSet(set<shared_ptr<Color> >({color1, color2, color3}));
 
     SECTION("Testing allContainsKmer") {
         string kmer1 = "AAA";

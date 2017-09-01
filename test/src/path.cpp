4841e4174492d50bc2bafceeef490593c857ba20
colelyman <cole@colelyman.com>
2017-09-01 00:03:49 -0600

    Updated the constructor for Paths.

--- test/src/path.cpp
+++ test/src/path.cpp
@@ -11,37 +11,19 @@ TEST_CASE("Path operations", "[path]") {
     Color zikaColor = Color(1, "zikaColor", "./data/zika.bwtdisk");
 
     SECTION("Shared Kmer Count") {
-        Path path1 = Path(&zikaColor);
-        Path path2 = Path(&zikaColor);
-
-        string seq1 = "ACT";
-        string seq2 = "TTG";
-       
-        path1.append(seq1);
-        path2.append(seq2);
+        Path path1 = Path("ACT");
+        Path path2 = Path("TTG");
 
         REQUIRE(path1.runSharedKmerCount(path2, 3) == 0);
 
-        Path path3 = Path(&zikaColor);
-        Path path4 = Path(&zikaColor);
-
-        string seq3 = "ACTGTCAGTGCAGTACAGTAACAAA";
-        string seq4 = "ACTGGCAGGGCACTATAGTAACAAT";
-
-        path3.append(seq3);
-        path4.append(seq4);
+        Path path3 = Path("ACTGTCAGTGCAGTACAGTAACAAA");
+        Path path4 = Path("ACTGGCAGGGCACTATAGTAACAAT");
 
         REQUIRE(path3.runSharedKmerCount(path4, 5) == 4);
 
         // Test the edge case to make sure the for loop gets the last kmer
-        Path path5 = Path(&zikaColor);
-        Path path6 = Path(&zikaColor);
-
-        string seq5 = "ACTGTCAGTGCAGTACAGTAACAAA";
-        string seq6 = "ACTGGCAGGGCACTATAGTAACAAA";
-
-        path5.append(seq5);
-        path6.append(seq6);
+        Path path5 = Path("ACTGTCAGTGCAGTACAGTAACAAA");
+        Path path6 = Path("ACTGGCAGGGCACTATAGTAACAAA");
 
         REQUIRE(path5.runSharedKmerCount(path6, 5) == 5);
     }
